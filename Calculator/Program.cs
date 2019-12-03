24bbe285ec9ebdd498e14eb78802efe922b4a4ab
e.alihangt@gmail.com <e.alihangt@gmail.com>
2019-12-03 21:41:35 +0600

    Add project files.

---
+++ Calculator/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int fn = int.Parse(args[0]);
+            int sn = int.Parse(args[1]);
+            Console.WriteLine(fn + sn);
+        }
+    }
+}
